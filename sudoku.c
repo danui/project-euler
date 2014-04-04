8915590387a8a26596768363b94e527194569999
jin <hsienjin@gmail.com>
2014-04-04 21:56:03 +1100

    WIP. sudoku.

--- sudoku.c
+++ sudoku.c
@@ -5,41 +5,67 @@
 #include "sudoku.h"
 
 struct sudoku {
-    int * value_grid;
+    int value_grid[9*9];
+    int candidate_grid[9*9*9];
+    int num_values;
+    //struct subproblem * subproblems;
+    int subproblem_grid[9*9*3];
 };
+
+
+
+void foreach_subproblem(struct sudoku * S, int i, int j,
+    void (*subproblem)(struct sudoku * S, int ib, int ic, int jb, int jc))
+{
+    subproblem(S, i, 1, 0, 9);
+    subproblem(S, 0, 9, j, 1);
+    subproblem(S, i/3*3, 3, j/3*3, 3);
+}
+
+
 
 struct sudoku * new_sudoku(void)
 {
     struct sudoku * S = calloc(1, sizeof(struct sudoku));
-    S->value_grid = calloc(81, sizeof(int));
+    sudoku_reset(S);
     return S;
 }
 
 void sudoku_free(struct sudoku * S)
 {
     if (S) {
-        free(S->value_grid);
         free(S);
     }
 }
 
 void sudoku_reset(struct sudoku * S)
 {
-    memset(S->value_grid, 0, 9*9);
+    int i, j, k;
+    memset(S, 0, sizeof(struct sudoku));
+    for (i=0; i<9; ++i)
+        for (j=0; j<9; ++j)
+            for (k=0; k<9; ++k)
+                S->candidate_grid[i*9*9 + j*9 + k] = 1;
 }
 
-void sudoku_put(struct sudoku * S, int i, int j, int value)
+void sudoku_put(struct sudoku * S, int I, int J, int value)
 {
-    S->value_grid[(i-1)*9+(j-1)] = value;
+    int i = I-1;
+    int j = J-1;
+    int k = value - 1;
+    S->value_grid[i*9+j] = value;
+    S->num_values += 1;
 }
 
-int sudoku_get(struct sudoku * S, int i, int j)
+int sudoku_get(struct sudoku * S, int I, int J)
 {
-    return S->value_grid[(i-1)*9+(j-1)];
+    int i = I-1;
+    int j = J-1;
+    return S->value_grid[i*9 + j];
 }
 
 void sudoku_solve(struct sudoku * S,
-    void (*on_put)(struct sudoku * S, int i, int j, int value))
+    void (*on_found)(struct sudoku * S, int I, int J, int value))
 {
     /* TODO: implement? */
 }
--- sudoku.h
+++ sudoku.h
@@ -9,17 +9,20 @@ void sudoku_free(struct sudoku * S);
 
 void sudoku_reset(struct sudoku * S);
 
-void sudoku_put(struct sudoku * S, int i, int j, int value);
+/**
+ * Put value at (I,J).  I and J are 1-based indices; i.e. 1..9
+ */
+void sudoku_put(struct sudoku * S, int I, int J, int value);
 
-int sudoku_get(struct sudoku * S, int i, int j);
+int sudoku_get(struct sudoku * S, int I, int J);
 
 /**
  * Solve sudoku problem S.
  *
- * @param on_put [in] If set, this callback will be invoked each time
+ * @param on_found [in] If set, this callback will be invoked each time
  * the solver finds a value to put.
  */
 void sudoku_solve(struct sudoku * S,
-    void (*on_put)(struct sudoku * S, int i, int j, int value));
+    void (*on_found)(struct sudoku * S, int I, int J, int value));
 
 #endif
