6de52f67b421929e0e0395fd76f6affd946e01a8
jin <hsienjin@gmail.com>
2014-04-05 13:29:09 +1100

    Update sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -8,19 +8,29 @@ struct sudoku * new_sudoku(void);
 void sudoku_free(struct sudoku * S);
 
 /**
- * Put value at (I,J).  I and J are 1-based indices; i.e. 1..9
+ * Put number at (row,col).  row and col are 0..8.
  */
-void sudoku_put(struct sudoku * S, int I, int J, int value);
+void sudoku_put(struct sudoku * S, int row, int col, int value);
 
-int sudoku_get(struct sudoku * S, int I, int J);
+/**
+ * @return number at (row,col).
+ */
+int sudoku_get(struct sudoku * S, int row, int col);
+
+struct sudoku_callbacks
+{
+    /**
+     * If provided, this callback will be invoked when sudoku_solve()
+     * finds a number to put into slot (row,col).
+     */
+    void (*on_set_number)(struct sudoku * S, int row, int col, int number);
+
+    /* more callbacks here, as we discover them... */
+};
 
 /**
  * Solve sudoku problem S.
- *
- * @param on_found [in] If set, this callback will be invoked each time
- * the solver finds a value to put.
  */
-void sudoku_solve(struct sudoku * S,
-    void (*on_found)(struct sudoku * S, int I, int J, int value));
+void sudoku_solve(struct sudoku * S, struct sudoku_callbacks * callbacks);
 
 #endif
