fd2f68986a27d80500fcf8f19124eb2a6835d366
jin <hsienjin@gmail.com>
2014-04-09 00:08:11 +1000

    Solution for problem 67

---
+++ problem-67.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define NROWS 100
+
+int * solve(int n, FILE * in)
+{
+    int * x = malloc(n*sizeof(int));
+    int * y;
+    int i;
+    for (i=0; i<n; ++i)
+        fscanf(in, "%d", x+i);
+    if (n < NROWS) {
+        y = solve(n+1, in);
+        for (i=0; i<n; ++i)
+            if (y[i] < y[i+1])
+                x[i] += y[i+1];
+            else
+                x[i] += y[i];
+        free(y);
+    }
+    return x;
+}
+
+int main(void)
+{
+    FILE * in = fopen("triangle.txt", "r");
+    int * x;
+    x = solve(1, in);
+    printf("x[0] = %d\n", x[0]);
+    return 0;
+}
