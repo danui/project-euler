b117dad189bdce8f7269b69c9ff26f32267c6169
jin <hsienjin@gmail.com>
2015-05-09 11:06:47 +0800

    Start working on problem 24 using Java.

---
+++ src/euler/Problem24.java
@@ -0,0 +1,12 @@
+package euler;
+
+import euler.Euler;
+
+public class Problem24 {
+
+    public static void main(String[] args) {
+        System.out.println("factorial(10) is " + Euler.factorial(10));
+        System.out.println("DONE");
+    }
+
+}
