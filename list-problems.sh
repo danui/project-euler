a7b7e1d03c2360162157098e19c6ebca21a938c9
jin <hsienjin@gmail.com>
2015-05-09 13:43:25 +0800

    Update list-problems.sh to search src dir.

--- list-problems.sh
+++ list-problems.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+function getNormalProblemIds {
+    ls problem-* | cut -d . -f1 | cut -d- -f2
+}
+
+function getJavaProblemIds {
+    ls src/euler/Problem*.java | cut -d. -f1 | cut -dm -f2
+}
+
+function getAllProblemIds {
+    getNormalProblemIds
+    getJavaProblemIds
+}
+
 function getProblemIds {
-    ls problem-* | cut -d . -f1 | cut -d- -f2 | sort -n | uniq
+    getAllProblemIds | sort -n | uniq
 }
 
 function listProblems {
     for i in $(getProblemIds); do
-	for j in c py scala java; do
+	for j in c py scala; do
 	    find . -name "problem-$i.$j"
 	    find . -name "problem-$i-*.$j"
 	done
+	find ./src -name "Problem$i.java"
     done
 }
 
