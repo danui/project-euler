299b071af03dc555e112d14279994ed07299b79e
jin <hsienjin@gmail.com>
2018-06-07 22:43:06 +0800

    Update list-problems.sh

--- list-problems.sh
+++ list-problems.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-function getNormalProblemIds {
-    ls problem-* | cut -d . -f1 | cut -d- -f2
-}
-
-function getJavaProblemIds {
-    ls src/euler/Problem*.java | cut -d. -f1 | cut -dm -f2
-}
-
-function getAllProblemIds {
-    getNormalProblemIds
-    getJavaProblemIds
-}
-
-function getProblemIds {
-    getAllProblemIds | sort -n | uniq
-}
-
-function listProblems {
-    for i in $(getProblemIds); do
-	for j in c py scala; do
-	    find . -name "problem-$i.$j"
-	    find . -name "problem-$i-*.$j"
-	done
-	find ./src -name "Problem$i.java"
-    done
-}
-
-listProblems
+find solutions -iname "problem*.c"
+find solutions -iname "problem*.java"
+find solutions -iname "problem*.py"
+find solutions -iname "problem*.ipynb" | grep -v checkpoint
